deb7bea347fd372e00c6b7d6e32a4326d50400b8
pjamsa <Paul_Jamsa@msn.com>
2021-04-21 10:53:06 -0700

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,14 @@
+class User: #parent class, 
+    name = ''
+    email = ''
+    password = ''
+    id = 0
+
+class Student(User) #child class
+    GPA = 3.2 #unique info about the Student class
+    Year = Freshamn
+
+class Teacher(User) #child class
+    Subject = Geography #unique info about the Teacher class
+    Years_Teaching = 7
+    
