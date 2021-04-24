23d5014ede6e9b530d7f4df408875ef8ac1739f0
pjamsa <Paul_Jamsa@msn.com>
2021-04-24 13:47:45 -0700

    Update Students_Func.py

--- Students/Students_Func.py
+++ Students/Students_Func.py
@@ -5,11 +5,11 @@ import sqlite3
 
 import Students
 
-conn = sqlite3.connect('Student_Tracking.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE if not exists tbl_students( \
+def create_db(self):
+    conn = sqlite3.connect('Students.db')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE if not exists tbl_students( \
             ID INTEGER PRIMARY KEY AUTOINCREMENT, \
             col_fname TEXT, \
             col_lname TEXT, \
@@ -18,8 +18,52 @@ with conn:
             col_email TEXT, \
             col_course TEXT \
             );")
-    conn.commit()
-conn.close
+        conn.commit()
+    conn.close()
+    first_run(self)
+
+
+def first_run(self):
+    conn = sqlite3.connect('Students.db')
+    with conn:
+        cur = conn.cursor()
+        cur,count = count_records(cur)
+        if count < 1:
+            cur.execute("""INSERT INTO tbl_students (col_fname,col_lname,col_fullname,col_phone,col_email,col_course) VALUES (?,?,?,?,?,?)""", ('John','Doe','John Doe','111-111-1111','jdoe@email.com','geography'))
+            conn.commit()
+    conn.close()
+
+
+def count_records(cur):
+    count = ""
+    cur.execute("""SELECT COUNT(*) FROM tbl_students""")
+    count = cur.fetchone()[0]
+    return cur,count
+
+#Select item in ListBox
+def onSelect(self,event):
+    #calling the event is the self.lstList1 widget
+    varList = event.widget
+    select = varList.curselection()[0]
+    value = varList.get(select)
+    conn = sqlite3.connect('Students.db')
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT col_fname,col_lname,col_phone,col_email,col_course FROM tbl_students WHERE col_fullname = (?)""", [value])
+        varBody = cursor.fetchall()
+        for data in varBody:
+            self.txt_fname.delete(0,END)
+            self.txt_fname.insert(0,data[0])
+            self.txt_lname.delete(0,END)
+            self.txt_lname.insert(0,data[1])
+            self.txt_phone.delete(0,END)
+            self.txt_phone.insert(0,data[2])
+            self.txt_email.delete(0,END)
+            self.txt_email.insert(0,data[3])
+            self.txt_course.delete(0,END)
+            self.txt_course.insert(0,data[4])
+
+
 
 
 
